76675bc02f89098df1a51f60b830addb35de64e3
Mario Slepcevic <sle3.14@gmail.com>
2022-07-19 14:22:35 +0200

    Add error handling to login

--- src/features/auth/fragments/AuthProvider.js
+++ src/features/auth/fragments/AuthProvider.js
@@ -1,5 +1,10 @@
 import { createContext, useCallback, useMemo, useState } from 'react'
-import { googleLogout, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google'
+import {
+  googleLogout,
+  GoogleOAuthProvider,
+  hasGrantedAllScopesGoogle,
+  useGoogleLogin,
+} from '@react-oauth/google'
 import PropTypes from 'prop-types'
 
 import { fetchWithAuth } from 'core/common'
@@ -8,13 +13,27 @@ export const AuthProviderContext = createContext()
 
 const InnerAuthProvider = ({ children }) => {
   const [accessToken, setAccessToken] = useState(null)
+  const [error, setError] = useState('')
 
   const isAuthorized = Boolean(accessToken)
 
   const login = useGoogleLogin({
     scope: `${process.env.REACT_APP_GOOGLE_API_SCOPE}`,
     onSuccess: response => {
-      setAccessToken(response.access_token)
+      const hasAccess = hasGrantedAllScopesGoogle(
+        response,
+        `${process.env.REACT_APP_GOOGLE_API_SCOPE}`,
+      )
+
+      if (hasAccess) {
+        setAccessToken(response.access_token)
+        setError('')
+      } else {
+        setError('Please grant access to your calendar.')
+      }
+    },
+    onError: () => {
+      setError('Login failed. Please try again.')
     },
   })
 
@@ -28,11 +47,12 @@ const InnerAuthProvider = ({ children }) => {
   const value = useMemo(
     () => ({
       authFetch,
+      error,
       isAuthorized,
       login,
       logout,
     }),
-    [authFetch, isAuthorized, login, logout],
+    [authFetch, error, isAuthorized, login, logout],
   )
 
   return <AuthProviderContext.Provider value={value}>{children}</AuthProviderContext.Provider>
--- src/features/login/fragments/Login.js
+++ src/features/login/fragments/Login.js
@@ -1,14 +1,22 @@
+import styled from 'styled-components'
+
 import { Button, Layout } from 'core/ui'
 
 import { useAuth } from 'features/auth'
 
 export const Login = () => {
-  const { login } = useAuth()
+  const { error, login } = useAuth()
 
   return (
     <Layout>
       <h1>Welcome</h1>
       <Button onClick={login}>Login</Button>
+      {error && <ErrorDiv>{error}</ErrorDiv>}
     </Layout>
   )
 }
+
+const ErrorDiv = styled.div`
+  margin-top: ${({ theme }) => theme.space.large};
+  color: red;
+`
